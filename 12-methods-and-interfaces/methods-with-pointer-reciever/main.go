cd944f63925bd5e399092031b9c3a067abaa0e7d
Rianto <rianto.rnt@gmail.com>
2022-09-22 14:11:11 +0800

    Methods with a pointer reciever - code explanations

--- 12-methods-and-interfaces/methods-with-pointer-reciever/main.go
+++ 12-methods-and-interfaces/methods-with-pointer-reciever/main.go
@@ -1,54 +1,69 @@
+/////////////////////////////////
+// Methods with a Pointer Receiver
+// Go Playground: https://play.golang.org/p/vyBZghOB5UM
+/////////////////////////////////
+
 package main
 
 import "fmt"
 
+// declaring a new struct type
 type car struct {
-	brand string
-	price int
+    brand string
+    price int
 }
 
-func changeCar(c car, newBrand string, newPrice int) {
-	c.price = newPrice
-	c.brand = newBrand
-}
-
+// declaring a method for car type
+// it changes the value it works on
 func (c car) changeCar1(newBrand string, newPrice int) {
-	c.brand = newBrand
-	c.price = newPrice
+    c.brand = newBrand
+    c.price = newPrice
+    // the changes are not seen to the outside world (pass by value)
 }
 
+// declaring a method with a pointer receiver
 func (c *car) changeCar2(newBrand string, newPrice int) {
-	(*c).brand = newBrand
-	(*c).price = newPrice
+    (*c).brand = newBrand
+    (*c).price = newPrice
+    // the changes are seen the outside world
+
 }
 
-// type distance *int
+// method declarations are not permitted on named types that are themselves pointer types
+type distance *int
 
-// func (d *distance) m1() {
-// 	fmt.Println("example rnt message")
+// ERROR ->  invalid receiver type *distance (distance is a pointer type)
+// func (d *distance) f() {
+//  fmt.Println("say something")
 // }
 
 func main() {
-	myCar := car{brand: "McLaren", price: 70000}
-	changeCar(myCar, "Ferrari", 100000)
-	fmt.Println(myCar)
 
-	myCar.changeCar1("Bugati", 21999)
-	fmt.Println(myCar)
+    // declaring a car value
+    myCar := car{brand: "Audi", price: 40000}
 
-	myCar.changeCar2("Dokar", 40000)
-	fmt.Println(myCar)
+    // calling the method with a value receiver
+    myCar.changeCar1("Opel", 21000)
 
-	var yourCar *car
-	yourCar = &myCar
-	fmt.Println(*yourCar)
+    // no change due to the same pass by value mechanism  !!!
+    fmt.Println(myCar) // => {Audi 40000}
 
-	// valid way
-	yourCar.changeCar2("Toyota", 20000)
-	fmt.Println(*yourCar)
+    // calling the method with a pointer receiver. It changes the value!
+    (&myCar).changeCar2("Seat", 25000)
+    fmt.Println(myCar) // -> {Seat 25000}
 
-	(*yourCar).changeCar2("More Toyota", 20000)
-	fmt.Println(*yourCar)
+    // declaring a pointer to car
+    var yourCar *car
+    yourCar = &myCar      // it stores the address of myCar
+    fmt.Println(*yourCar) // -> {Seat 25000}
 
-	fmt.Println(myCar)
+    // calling the method on pointer type
+    // valid ways to call the method:
+    yourCar.changeCar2("VW", 30000)
+    (*yourCar).changeCar2("VW", 30000)
+    fmt.Println(*yourCar) // => {VW 30000}
+
+    // in the above example both myCar and yourCar variables have been modified.
+    fmt.Println(myCar) // => {VW 30000}
+
 }
