c5267c26540ea3852ab3da83fb7660330ba16170
Rianto <rianto.rnt@gmail.com>
2022-09-26 23:09:16 +0800

    Mutexes - code explanation

--- 13-concurrency/mutexes/main.go
+++ 13-concurrency/mutexes/main.go
@@ -1,3 +1,15 @@
+/////////////////////////////////
+// Mutexes
+// Go Playground: https://play.golang.org/p/xVBcUn-CS_4
+/////////////////////////////////
+
+//** IMPORTANT **//
+// Run this program on your local machine (not in Go Playground)
+// Execute: go run main.go
+
+// Use Go Race Detector to check that there is no Data Race
+// Execute: go run -race main.go
+
 package main
 
 import (
@@ -7,37 +19,47 @@ import (
 )
 
 func main() {
-	const gr = 100
 
-	var wg sync.WaitGroup
-	wg.Add(gr * 2)
+    const gr = 100
+    var wg sync.WaitGroup
+    wg.Add(gr * 2)
 
-	var n int = 0
+    // declaring a shared value
+    var n int = 0
 
-	// 1) Mutex
-	var m sync.Mutex
+    // 1.
+    // Declaring a mutex. It's available in sync package
+    var m sync.Mutex
 
-	for i := 0; i < gr; i++ {
-		go func ()  {
-			time.Sleep(time.Second / 10)
-			// 2) Mutex
-			m.Lock()
-			n++
-			m.Unlock()
-			wg.Done()
-		}()	
+    for i := 0; i < gr; i++ {
+        go func() {
+            time.Sleep(time.Second / 10)
 
-		go func ()  {
-			time.Sleep(time.Second / 10)
-			m.Lock()
-			defer m.Unlock()
-			n--
-			// m.Unlock()
-			wg.Done()
-		}()		
-	}
+            // 2.
+            // Lock the access to the shared value
+            m.Lock()
+            n++
 
-	wg.Wait()
+            // 3.
+            // Unlock the variable after it's incremented
+            m.Unlock()
 
-	fmt.Println("Final value of n:", n)
+            wg.Done()
+        }()
+
+        // Doing the same for the 2nd goroutine
+        go func() {
+            time.Sleep(time.Second / 10)
+            m.Lock()
+            defer m.Unlock()
+            n--
+            wg.Done()
+        }()
+
+    }
+
+    wg.Wait()
+
+    // printing the final value of n
+    fmt.Println(n) // the final final of n will be always 0
 }
