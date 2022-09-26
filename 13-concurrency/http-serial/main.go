b51ff58c143b534df0e5521aee9f531488ffc74b
Rianto <rianto.rnt@gmail.com>
2022-09-26 22:21:51 +0800

    Project_ URL Checker and Page Downloader - code explanation

--- 13-concurrency/http-serial/main.go
+++ 13-concurrency/http-serial/main.go
@@ -9,24 +9,35 @@ import (
 )
 
 func checkAndSaveBody(url string) {
+
+	// calling http.Get() which gives a response and an error value.
 	resp, err := http.Get(url)
 
+	// error handling
 	if err != nil {
-		fmt.Println(err)
 		fmt.Printf("%s is DOWN!\n", url)
 	} else {
+		// a good practice is to close the Response Body if there is one
+		// If you forget to close it there will be a resource leak.
 		defer resp.Body.Close()
-		fmt.Printf("%s -> Status code: %d \n", url, resp.StatusCode)
+		fmt.Printf("%s -> Status Code: %d  \n", url, resp.StatusCode)
+
+		// fetching the page if HTTP Status Code is 200 (success)
 		if resp.StatusCode == 200 {
 
-			bodyByte, err := ioutil.ReadAll(resp.Body)
+			// The resp.Body implements the io.Reader interface
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+			// Creating the file's name
 			file := strings.Split(url, "//")[1]
-			file += ".txt"
+			file += ".txt" // and I concatenate .txt to file value
 
-			fmt.Printf("Writing res body to %s\n", file)
+			fmt.Printf("Writing response Body to %s\n", file)
 
-			err = ioutil.WriteFile(file, bodyByte, 0664)
-
+			// Writing the response Body to File
+			// If the file doesn't exist WriteFile() creates it and if it already exists
+			// the function will truncate it before writing to file.
+			err = ioutil.WriteFile(file, bodyBytes, 0664)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -35,10 +46,23 @@ func checkAndSaveBody(url string) {
 }
 
 func main() {
-	urls := []string{"https://medium.com", "https://google.com", "https://bing.com"}
+	urls := []string{"https://www.golang1.org", "https://www.google.com", "https://www.medium.com"}
 
+	// Iterating over the URLs and call the function for each URL
 	for _, url := range urls {
 		checkAndSaveBody(url)
 		fmt.Println(strings.Repeat("#", 20))
 	}
 }
+
+// Run the program: go run main.go
+
+//** EXPECTED OUTPUT: **//
+// https://www.golang1.org is DOWN!
+// ####################
+// https://www.google.com -> Status Code: 200
+// Writing response Body to www.google.com.txt
+// ####################
+// https://www.medium.com -> Status Code: 200
+// Writing response Body to www.medium.com.txt
+// ####################
