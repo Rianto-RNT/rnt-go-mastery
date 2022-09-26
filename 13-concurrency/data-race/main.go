1e8b39bbb56fa385d17182428c62b3f60eb06dc2
Rianto <rianto.rnt@gmail.com>
2022-09-26 23:02:20 +0800

    Go race detector - code explanation

--- 13-concurrency/data-race/main.go
+++ 13-concurrency/data-race/main.go
@@ -1,3 +1,15 @@
+/////////////////////////////////
+// Data Races in Go
+// Go Playground:https://play.golang.org/p/AiSjfvn4O3T
+/////////////////////////////////
+
+//** IMPORTANT **//
+// Run this program on your local machine (not in Go Playground)
+// Execute: go run main.go
+
+// Use Go Race Detector to detect the Data Race
+// Execute: go run -race main.go
+
 package main
 
 import (
@@ -7,28 +19,45 @@ import (
 )
 
 func main() {
-	const gr = 100
 
-	var wg sync.WaitGroup
-	wg.Add(gr * 2)
+    const gr = 100
 
-	var n int = 0
+    // declaring a WaitGroup to synchronize the goroutines with the main function.
+    var wg sync.WaitGroup
 
-	for i := 0; i < gr; i++ {
-		go func ()  {
-			time.Sleep(time.Second / 10)
-			n++
-			wg.Done()
-		}()	
+    // adding 200 goroutines to the WaitGroup
+    wg.Add(gr * 2)
 
-		go func ()  {
-			time.Sleep(time.Second / 10)
-			n--
-			wg.Done()
-		}()		
-	}
+    // declaring a shared value
+    var n int = 0
 
-	wg.Wait()
+    // starting 200 goroutines
+    for i := 0; i < gr; i++ {
 
-	fmt.Println("Final value of n:", n)
+        // each goroutine is an anonymous function
+        go func() {
+
+            // introducing some artificial time
+            time.Sleep(time.Second / 10)
+
+            // increment the shared value
+            n++
+
+            // notifying the WaitGroup that the goroutine is done
+            wg.Done()
+        }()
+
+        // goroutine that decrements the shared value
+        go func() {
+            time.Sleep(time.Second / 10)
+            n--
+            wg.Done()
+        }()
+
+    }
+    // waiting for the goroutines to terminate.
+    wg.Wait()
+
+    //  printing the final value of n
+    fmt.Println(n) // it will have another value for each program execution -> DATA RACE
 }
