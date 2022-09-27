5bd90582af3633cc6badc1f6595c0536b047a600
Rianto <rianto.rnt@gmail.com>
2022-09-27 19:02:32 +0800

    Unbuffered channels - Code explanation

--- 13-concurrency/channels-unbuffered/main.go
+++ 13-concurrency/channels-unbuffered/main.go
@@ -1,3 +1,8 @@
+/////////////////////////////////
+// Unbuffered Channels
+// Go Playground: https://play.golang.org/p/_44csjQDJvM
+/////////////////////////////////
+
 package main
 
 import (
@@ -6,24 +11,32 @@ import (
 )
 
 func main() {
-	c1 := make(chan int)    // unbuffered channels
-	c2 := make(chan int, 3) // buffered channels
-	_ = c2
+	c1 := make(chan int) //unbuffered channel
 
+	// Launching a goroutine
 	go func(c chan int) {
-		fmt.Println("func gorutine START sending data into the channel")
+		fmt.Println("func goroutine starts sending data into the channel")
 		c <- 10
-		fmt.Println("func gorutine AFTER sending data into the channel")
+		fmt.Println("func goroutine after sending data into the channel")
+	}(c1) // calling the anonymous func and passing c1 as argument
 
-	}(c1)
-
-	fmt.Println("Main gorutine sleeps for 2 seconds")
+	fmt.Println("main goroutine sleeps for 2 seconds")
 	time.Sleep(time.Second * 2)
 
-	fmt.Println("main gorutine START recieving data")
+	fmt.Println("main goroutine starts receiving data")
 	d := <-c1
-	fmt.Println("main gorutine recieving data:", d)
+	fmt.Println("main goroutine received data:", d)
 
+	// we sleep for a second to give time to the goroutine to finish
 	time.Sleep(time.Second)
 
+	// After running the program we notice that the sender (the func goroutine) blocks on the channel
+	// until the receiver (the main goroutine) receives the data from the channel.
 }
+
+//** EXPECTED OUTPUT: **//
+// main goroutine sleeps for 2 seconds
+// func goroutine starts sending data into the channel
+// main goroutine starts receiving data
+// main goroutine received data: 10
+// func goroutine after sending data into the channel
